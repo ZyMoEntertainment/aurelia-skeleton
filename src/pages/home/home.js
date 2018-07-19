79fdd1ff41d5d29681a19ab7bf1962085f40a62b
Collin Herber <collin.herber@zymoent.com>
2018-07-19 14:22:23 -0500

    readme fix and code minification

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -13,7 +13,14 @@ export class Home {
     }
 
     attached() {
+        //The attached function is going to fire BEFORE the page is loaded
+    }
 
+    async activate() {
+        //The activate function is going to fire AFTER the page is loaded
+        //Typically this is where you would want to do any routes to bring in the data for a page.
+        //If this data is going to take awhile to load, I would do it in the attached.
+        //Aurelia supports ES6/7 Methods so using async/await is going to be your best friend
     }
 
     rangeSliderValue = 50; //We use this variable to store our selected value from the input slider
@@ -42,14 +49,11 @@ export class Home {
         }
     }
 
-    openNotification(type) {
-        if (type === 'info') {
-            this.notification.info('Some Info Message!')
-        } else if (type === 'error') {
-            this.notification.error('Some Error!')
-        } else {
-            this.notification.success('Some Success Message!')
-        }
+    openNotification(button) {
+        this.notification[button.type](button.message)
+        //FOR YOU JAVASCRIPT NUBS:
+        // IF button = {type: 'success', message: "Open Success Message"}
+        // this.notification[button.type](button.message) translates to this.notification.success("Open Success Message")
     }
 
 }
